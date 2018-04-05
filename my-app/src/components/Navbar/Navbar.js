6d168e8cff631ffecb41d8ea37e0c757eb196177
Preston McNabb <prestonmcnabb@Prestons-MacBook-Pro.local>
2018-04-05 18:02:40 -0500

    Update home page css

--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -4,30 +4,24 @@ import "./Navbar.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 const Navbar = props => (
-  <nav className="navbar navbar-default">
+  <nav className="navbar navbar-inverse navbar-static-top">
     <div className="container-fluid">
       <div className="navbar-header">
         <Link className="navbar-brand" to="/">
-          Pupster
+          Code&Brew
         </Link>
       </div>
       <ul className="nav navbar-nav">
-        <li
-          className={
-            window.location.pathname === "/" || window.location.pathname === "/about"
-              ? "active"
-              : ""
-          }
-        >
-          <Link to="/">About</Link>
-        </li>
         <li className={window.location.pathname === "/discover" ? "active" : ""}>
-          <Link to="/discover">Discover</Link>
+          <Link to="/discover">About</Link>
         </li>
         <li className={window.location.pathname === "/search" ? "active" : ""}>
           <Link to="/search">Search</Link>
         </li>
       </ul>
+      <button type="submit" className={window.location.pathname === "/search" ? "active" : "btn btn-info"}>
+        <Link to="/search">Log In</Link>
+      </button>
     </div>
   </nav>
 );
