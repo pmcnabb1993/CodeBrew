cfbccb4a88215a8ef58ac5e5b2e248119ca57209
Preston McNabb <prestonmcnabb@Prestons-MacBook-Pro.local>
2018-04-10 18:22:37 -0500

    updated css on multiple files to fix some issues

--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css";
 
 
 
-// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+
 const Navbar = props => (
   <nav className="navbar navbar-default navbar-static-top">
     <div className="container-fluid">
