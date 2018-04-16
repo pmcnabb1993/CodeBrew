5252ae088d79b423772212fed19183464e30f0c5
Preston McNabb <prestonmcnabb@Prestons-MacBook-Pro.local>
2018-04-16 15:50:42 -0500

    Created react-open weather npm component and updated css

--- my-app/src/Home/Home.js
+++ my-app/src/Home/Home.js
@@ -3,17 +3,20 @@ import Hero from "../components/Hero";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
+import Weather from "../components/Weather";
+
 
 class Home extends Component {
   render() {
     const { isAuthenticated, login } = this.props.auth;
     return (
       <div>
-    <Hero backgroundImage="https://images.unsplash.com/photo-1521342475957-8db764a86913?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ae19afb005f9f45df7170938bc3a661a&auto=format&fit=crop&w=2851&q=80">
-      <h1> Code '&' Brew </h1>
-      <h2>We help with all your coding needs.</h2>
-    </Hero>
-    <Container style={{ marginTop: 30 }}>
+      <Weather />
+      <Hero backgroundImage="https://images.unsplash.com/photo-1521342475957-8db764a86913?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ae19afb005f9f45df7170938bc3a661a&auto=format&fit=crop&w=2851&q=80">
+        <h1> Code '&' Brew </h1>
+        <h2>We help with all your coding needs.</h2>
+      </Hero>
+      <Container style={{ marginTop: 30 }}>
       <Row>
         <Col size="md-12">
           <h1>Your Project ToDos!</h1>
@@ -21,9 +24,8 @@ class Home extends Component {
       </Row>
       <Row>
         <Col size="md-12">
-         
           <p>
-            Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum porttitor. Sed malesuada
+            Hey there. Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum porttitor. Sed malesuada
             molestie velit ac viverra. Quisque a ullamcorper purus. Curabitur luctus mi ac mi
             hendrerit semper. Nulla tincidunt accumsan lobortis. Mauris convallis sapien non nibh
             porta accumsan. Nunc volutpat tempus porttitor. Nunc congue dictum egestas. Aliquam
