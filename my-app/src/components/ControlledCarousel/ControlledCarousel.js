f6c6821303fea7858ab8ea4fcf8aa0283c70f915
Preston McNabb <prestonmcnabb@Prestons-MacBook-Pro.local>
2018-04-17 18:24:09 -0500

    Added carousel feature to main page, pictures routate through

---
+++ my-app/src/components/ControlledCarousel/ControlledCarousel.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { Row, Col, Carousel, CarouselItem, CarouselCaption } from 'react-bootstrap';
+import styles from "./ControlledCarousel.css";
+
+
+class ControlledCarousel extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleSelect = this.handleSelect.bind(this);
+
+    this.state = {
+      index: 0,
+      direction: null
+    };
+  }
+
+  handleSelect(selectedIndex, e) {
+    
+    this.setState({
+      index: selectedIndex,
+      direction: e.direction
+    });
+  }
+
+  render() {
+    const { index, direction } = this.state;
+
+    return (
+      <Carousel
+        activeIndex={index}
+        direction={direction}
+        onSelect={this.handleSelect}
+      >
+        <Carousel.Item>
+          <img   alt="computercoffee" src="https://images.unsplash.com/photo-1521342475957-8db764a86913?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ae19afb005f9f45df7170938bc3a661a&auto=format&fit=crop&w=2851&q=80" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img  alt="900x500" src="https://images.unsplash.com/photo-1520881363902-a0ff4e722963?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=eaaa6e11b146f75c1a685578d3c89dff&auto=format&fit=crop&w=1350&q=80" />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img  alt="900x500" src="https://images.unsplash.com/photo-1516342243255-ac2202f9f149?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=75dacc47b95222986df91d7786c40b25&auto=format&fit=crop&w=1350&q=80" />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+  }
+}
+
+export default ControlledCarousel;
